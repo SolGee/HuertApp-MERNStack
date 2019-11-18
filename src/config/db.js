b455bf402a8bf6b2c23191760d5c01f6f856a26e
SolGee <solsv00@hotmail.com>
2019-11-18 12:23:36 -0600

    Creating config folder. Initializing mongooseURI with our DB url(default.json); Connecting DB (db.js)

---
+++ src/config/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+
+//The method .get allow us to get anything we require form the module as an argument
+const db = config.get('mongoURI');
+
+const connectDB = async() => {
+    try {
+        //DeprecationWarning: current URL string parser is deprecated, and will be removed in a future version. To use the new parser, pass option { useNewUrlParser: true } to MongoClient.connect.
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('MongoDB Connected...');
+
+    } catch (err) {
+        console.error(err.message);
+
+        //Exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
