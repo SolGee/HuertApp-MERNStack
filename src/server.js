1c1677ff50c4b72540cfc34bf82df4c26b754e88
SolGee <solsv00@hotmail.com>
2019-11-18 14:30:09 -0600

    Create params for user validation using express-validator/check.

--- src/server.js
+++ src/server.js
@@ -4,8 +4,12 @@ const connectDB = require('./config/db');
 const app = express();
 
 //Connect DB
-
 connectDB();
+
+//Init middleware
+app.use(express.json({
+    extended: false
+}))
 
 app.get('/', (req, res) => res.send('API Running.'));
 
