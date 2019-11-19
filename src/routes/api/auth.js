d6ea8905501926b46a82a8cd1b92c138c12fbff5
SolGee <solsv00@hotmail.com>
2019-11-18 19:36:33 -0600

    Create middleware auth function to verify if the user has a token and return the user info (-)pass from the DB.

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
 const router = express.Router();
+//This will help us if we use it as a parameter to limit the routes if there is not al authentication
+const auth = require('../../middleware/auth');
 
-
+const User = require('../../models/User');
 //@router   GET api/auth
 //@desc     Test route
 //@acces    Public
-router.get('/', (req, res) => {
-    res.send('Auth Route')
+router.get('/', auth, async(req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
