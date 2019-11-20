b1d585555ffab5fd94ad576daff4f19a2d1e0a0d
SolGee <solsv00@hotmail.com>
2019-11-19 21:36:47 -0600

    Create profile user route. Miss Profile.

--- src/routes/api/profile.js
+++ src/routes/api/profile.js
@@ -1,13 +1,26 @@
 const express = require('express');
-
+const auth = require('../../middleware/auth');
 const router = express.Router();
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
+//@router   GET api/profile/me
+//@desc     Get current users profiles
+//@acces    Private
+router.get('/me', auth, async(req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate('user', ['name', 'avatar']);
 
-//@router   GET api/profile
-//@desc     Test route
-//@acces    Public
-router.get('/', (req, res) => {
-    res.send('Profile Route')
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
