e47719b1be66f7a1d24149a937f377b62e29bdc7
SolGee <solsv00@hotmail.com>
2019-11-20 16:40:40 -0600

    Create function to get an user's profile using the ID

--- src/routes/api/profile.js
+++ src/routes/api/profile.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const auth = require('../../middleware/auth');
 const router = express.Router();
-const {
-    check,
-    validationResult
-} = require('express-validator/check');
+const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -34,7 +31,17 @@ router.get('/me', auth, async(req, res) => {
 //@desc     Createor update user profile
 //@acces    Private
 
-router.post('/', [auth, [check('location', 'Location is required').not().isEmpty(), check('skills', 'Skills are required').not().isEmpty()]],
+router.post(
+    '/', [
+        auth, [
+            check('location', 'Location is required')
+            .not()
+            .isEmpty(),
+            check('skills', 'Skills are required')
+            .not()
+            .isEmpty()
+        ]
+    ],
     async(req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -73,7 +80,6 @@ router.post('/', [auth, [check('location', 'Location is required').not().isEmpty
         if (facebook) profileFields.social.facebook = facebook;
         if (instagram) profileFields.social.instagram = instagram;
 
-
         try {
             // Using upsert option (creates new doc if no match is found):
             let profile = await Profile.findOneAndUpdate({
@@ -89,8 +95,63 @@ router.post('/', [auth, [check('location', 'Location is required').not().isEmpty
             console.error(err.message);
             res.status(500).send('Server Error');
         }
+    }
+);
 
+//@router   Get api/profile
+//@desc     Get all profiles
+//@acces    Public
+router.get('/', async(req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
-    });
+//@router   Get api/profile/user/:user_id
+//@desc     Get profile by ID
+//@acces    Public
+router.get('/user/:user_id', async(req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.params.user_id
+        }).populate('user', ['name', 'avatar']);
+        if (!profile) {
+            return res.status(400).json({
+                msg: 'Profile not found.'
+            });
+        }
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({
+                msg: 'Profile not found.'
+            });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+//@router   Delete api/profile
+//@desc     Delete profile, user & post
+//@acces    Private
+router.delete('/', auth, async(req, res) => {
+    try {
+        //Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+
+        //Remove users
+        await User.findOneAndRemove({ user: req.user.id });
+
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
