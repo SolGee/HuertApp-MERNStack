6e1e3c2afce2346d8cef72ca60232fec66316af2
SolGee <solsv00@hotmail.com>
2019-11-23 23:21:59 -0600

    Add huertapp favicon

---
+++ src/huertapp-fe/src/components/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <figure>
+          <img src='./assets/logo.svg' alt='huertapp-logo' />
+        </figure>
+      </nav>
+    </header>
+  );
+};
