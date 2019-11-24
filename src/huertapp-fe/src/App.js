db413cce0c048f5505502f0c91be92e20e236f78
SolGee <solsv00@hotmail.com>
2019-11-24 00:39:18 -0600

    React Router Setup

--- src/huertapp-fe/src/App.js
+++ src/huertapp-fe/src/App.js
@@ -1,12 +1,18 @@
 import React, { Fragment } from 'react';
-import Navbar from './components/layout/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+      </Fragment>
+    </Router>
   );
 };
 
--- src/huertapp-fe/src/components/layout/Landing.jsx
+++ src/huertapp-fe/src/components/layout/Landing.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Login from '../auth/Login';
+import Register from '../auth/Register';
 
 export const Landing = () => {
-  return div;
+  return (
+    <Fragment>
+      <main className='container'>
+        <aside className='orange'></aside>
+        <section className='register-container'>
+          <Login />
+          <Register />
+        </section>
+      </main>
+    </Fragment>
+  );
 };
+export default Landing;
