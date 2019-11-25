d9ce275890d2b23b51d987a3976578160e487702
SolGee <solsv00@hotmail.com>
2019-11-24 18:10:32 -0600

    Redux store setup

--- src/huertapp-fe/src/App.js
+++ src/huertapp-fe/src/App.js
@@ -2,17 +2,24 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
+//Redux
+import { Provider } from 'react-redux';
+import Store from './store';
+
+//Components
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-      </Fragment>
-    </Router>
+    <Provider store={Store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
